f723391f4ca47e2ff4ae5c63a828e19d4c401383
amit03 <amitghosh0595c@gmail.com>
2024-09-01 14:37:53 +0530

    first deploy

---
+++ src/context/socketContext.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+interface SocketContextProps {
+  socket: Socket | null;
+}
+
+const SocketContext = createContext<SocketContextProps | undefined>(undefined);
+
+export const useSocket = (): SocketContextProps => {
+  const context = useContext(SocketContext);
+  if (!context) {
+    throw new Error('useSocket must be used within a SocketProvider');
+  }
+  return context;
+};
+
+interface SocketProviderProps {
+  children: ReactNode;
+}
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const socketInstance = io('http://localhost:8080');
+    setSocket(socketInstance);
+
+    socketInstance.on('connect', () => {
+      console.log('Socket connected:', socketInstance.id);
+    });
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
